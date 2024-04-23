01946c4bf69622b5f5522fa2fbaeb130aefde1a7
Evgen Chap <evchap8888@gmail.com>
2024-04-23 16:44:46 +0300

    Adding the "Add" form.

--- Django_Project_virt_1/bboard/urls.py
+++ Django_Project_virt_1/bboard/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from bboard.views import index, by_rubric
+from bboard.views import index, by_rubric, BbCreateView
 
 urlpatterns = [
+    path('add/', BbCreateView.as_view(), name='add'), # стр 64
 #    path('<int:rubric_id>/', by_rubric),# 57
     path('<int:rubric_id>/', by_rubric, name='by_rubric'), # стр 61
     path('', index, name='index'),
--- Django_Project_virt_1/bboard/views.py
+++ Django_Project_virt_1/bboard/views.py
@@ -46,3 +46,15 @@ def by_rubric(request, rubric_id): # 57
     context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric':current_rubric}
     return render(request, 'bboard/by_rubric.html', context)
 
+from django.views.generic.edit import CreateView
+from .forms import BbForm
+
+class BbCreateView(CreateView): # стр 62
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    success_url = '/bboard/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
