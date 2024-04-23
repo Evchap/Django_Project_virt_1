7d8266babbb4d14cdecc9330e5039f18119333fd
Evgen Chap <evchap8888@gmail.com>
2024-04-23 15:56:30 +0300

    Обратное разрешение интернет-адресов.

--- Django_Project_virt_1/bboard/urls.py
+++ Django_Project_virt_1/bboard/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from bboard.views import index, by_rubric
 
-urlpatterns = [ # 57
-    path('<int:rubric_id>/', by_rubric),
+urlpatterns = [
+#    path('<int:rubric_id>/', by_rubric),# 57
+    path('<int:rubric_id>/', by_rubric, name='by_rubric'), # стр 61
     path('', index),
 ]
